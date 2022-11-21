16547e28ee7f11e41fbfe4c5d94319b59df3e470
khm <gud3308@naver.com>
2022-11-21 12:22:00 +0900

    vscode one commit

---
+++ web.py
@@ -0,0 +1,15 @@
+print('hello')
+#데이터 형
+#변수
+#데이터 구조(리스트, 딕셔너리, 튜플, 집합)
+#제어문 (조건문, 반복문)
+#클래스 함수
+
+a = 10
+b = 20
+print(a+b)
+
+num1 = int(input('input num1 : '))
+num2 = int(input('input num2 : '))
+# f-string (python 3.x)
+print(f"{num1} + {num2} = {num1 + num2}")
